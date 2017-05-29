7f1b4f969507409aa6c413e52ffa43c068b17299
Héctor Marcos Rabadán <hmarcos@ucm.es>
2017-05-29 11:45:07 +0200

    Creado test cambiar dir

---
+++ TestCambiaDIR/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Pac_Man;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestCambiaDIR
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CambiaADerecha()
+        {
+            Tablero tab = new Tablero(3, 3);
+            tab.setPersonaje(0, 1, 1, 0, 1);
+            
+        }
+    }
+}
